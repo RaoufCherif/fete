23adbb226388974e2f79cc70d68f4a6f7fd1ba25
raouf <yahia19raouf@gmail.com>
2023-05-05 18:32:55 +0200

    react app

---
+++ src/main/java/fr/r4s/fete/FeteApplication.java
@@ -0,0 +1,16 @@
+package fr.r4s.fete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FeteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeteApplication.class, args);
+	}
+
+
+
+}
